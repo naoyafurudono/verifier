06977997230bd3a70ba3d28ce1db970ef71a0e1f
naoyafurudono <naoyafurudono@gmail.com>
2023-01-06 17:27:05 +0900

    WIP impl gen_script

---
+++ src/gen_script.py
@@ -0,0 +1,20 @@
+from inst import EndInst, Instruction, SortInst
+from typing import Generator
+from check import Context, Definition
+from parse import StarTerm, Term, VarTerm
+
+
+def run(
+    t: Term, env: list[Definition], ctx: Context
+) -> Generator[Instruction, None, None]:
+    for inst in gen_script(t, env, ctx):
+        yield inst
+    yield EndInst(-1)
+
+
+def gen_script(
+    t: Term, env: list[Definition], ctx: Context
+) -> Generator[Instruction, None, None]:
+    if isinstance(t, StarTerm):
+        yield SortInst(-2)
+    yield Instruction(-2)
